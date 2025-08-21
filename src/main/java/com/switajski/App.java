1a9b8a99ba36e6855748721032edcf4f9111beb6
Marek Switajski <marek.switajski.ext@puma.com>
2025-08-21 10:58:25 +0200

    IntelliJ shows warning with Lombok's @NonNull

--- src/main/java/com/switajski/App.java
+++ src/main/java/com/switajski/App.java
@@ -1,5 +1,7 @@
 package com.switajski;
 
+import lombok.NonNull;
+
 import java.util.Objects;
 
 /**
@@ -10,12 +12,17 @@ public class App {
         App app = new App();
         app.nonNull(null);
         app.derivedNonNull(new Something().getNull());
+        app.derivedNonNullWithLombokNonNull(new Something().getNull());
     }
 
     public void nonNull(Object value) {
         Objects.requireNonNull(value);
     }
     public void derivedNonNull(Object value) {
+        Objects.requireNonNull(value);
+    }
+
+    public void derivedNonNullWithLombokNonNull(@NonNull Object value) {
         Objects.requireNonNull(value);
     }
 
